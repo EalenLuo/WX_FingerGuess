754e5594e0b017c7ffb0ddfcdc3f1f5f9022465c
ealenLuo <ealenluo@ealenLuodeMacBook-Pro.local>
2018-07-12 16:09:19 +0800

    加上‘猜拳游戏’的逻辑判断处理

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,9 @@
 //index.js
 //获取应用实例
 const app = getApp()
+let timer;
+let aiImgIndex;
+var resultStr = "比赛结果即将揭晓";
 
 Page({
 
@@ -8,6 +11,12 @@ Page({
    * 页面的初始数据
    */
   data: {
+    imgAi: '',
+    imgUser: '',
+    winCount:0,
+    failedCount:0,
+    gameResult: '',
+    playAgain: false,
     src:[
       '../img/clipper.png',
       '../img/step.png',
@@ -19,55 +28,67 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.setData({ imgUser: '../img/default.png'});
+    this.setData({winCount:0});
+    this.setData({gameResult : '比赛结果即将揭晓'});
+    this.startTimer();
+  },
+  startTimer: function(){
+    timer = setInterval(this.changeAiImg, 50);
+  },
+  stopTimer(){
+    clearInterval(timer);
+  },
+  changeAiImg: function(){
+    aiImgIndex = parseInt(Math.random() * 3);
+    this.setData({imgAi: this.data.src[aiImgIndex]});
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  myChoose: function (event){
+
+    if(this.data.playAgain){
+      return;
+    }
+    // 获取当前选中的索引，并将该索引的图片赋值用户选择的图片
+    let chooseIndex = event.currentTarget.id;
+    this.setData({imgUser: this.data.src[chooseIndex]});
+    this.stopTimer();
+
+    if (this.data.imgAi == this.data.imgUser){
+      resultStr = '不相上下，打个平局';
+    }
+    else if (this.data.imgAi == '../img/clipper.png' && this.data.imgUser == '../img/stone.png'){
+      this.setData({ winCount : this.data.winCount + 1});
+      resultStr = '🐂逼少年，你赢了';
+    }
+    else if (this.data.imgAi == '../img/stone.png' && this.data.imgUser == '../img/step.png') {
+      this.setData({ winCount: this.data.winCount + 1 });
+      resultStr = '🐂逼少年，你赢了';
+    }
+    else if (this.data.imgAi == '../img/step.png' && this.data.imgUser == '../img/clipper.png') {
+
+      this.setData({ winCount: this.data.winCount + 1 });
+      resultStr = '🐂逼少年，你赢了';
+    }else {
+      
+      this.setData({ failedCount: this.data.failedCount + 1});
+      resultStr = '噢噢，少年你输了';
+    }
+
+    this.setData({gameResult : resultStr});
+    this.setData({playAgain: true});
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
+  playAgain: function(){
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+    if(this.data.playAgain == false){
+      return;
+    }
+    this.startTimer();
+    resultStr = "比赛结果即将揭晓";
+    this.setData({imgUser : '../img/default.png'});
+    this.setData({ gameResult: resultStr});
+    this.setData({ playAgain: false });
   }
+
 })
